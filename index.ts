9b847853cf01f73a2293ad262e3c96f409bc73a7
Nattapon Rattajak <rattajak.n@gmail.com>
2018-05-25 16:57:30 +0700

    export all to index file.

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from "./src/IConnectOptions";
+export * from "./src/IEncodingParameters";
+export * from "./src/IParticipant";
+export * from "./src/IRoom";
+export * from "./src/ITrack";
+export * from "./src/videoCall";
